5d87f60fbf39e5c75e86fd08ea0add8687cfad32
Bhavyeshshah <81833413+Bhavyeshshah@users.noreply.github.com>
2021-11-28 14:00:00 +0530

    25th Commit

--- Recipe/settings.py
+++ Recipe/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ==============================================================================
 
 SECRET_KEY = config('SECRET_KEY', default=string.ascii_letters)
-ALLOWED_HOSTS = config['*']
+ALLOWED_HOSTS = ['*']
 
 DEBUG = config('DEBUG', default=True, cast=bool)
 
